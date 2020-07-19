86de4a13fc10f551597ad174ec839d9af50b0b49
Fabrizio Fallico <me@fabriziofallico.com>
2020-07-20 00:53:17 +0200

    feat: updated to 2020.7.14

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-    
+  
 from setuptools import setup
 
 
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['pytype==2020.05.13'],
+    install_requires=['pytype==2020.7.14'],
 )
